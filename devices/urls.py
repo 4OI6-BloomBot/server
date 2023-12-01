701c761193b56b3c7e69c3025e9b8a39adc18c61
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2023-12-01 00:10:24 -0500

    Add device editing

--- devices/urls.py
+++ devices/urls.py
@@ -8,6 +8,7 @@ from . import views
 
 app_name    = 'devices' # Register namespace
 urlpatterns = [
-    path("",          views.IndexView.as_view(),  name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("",               views.IndexView.as_view(),  name="index"),
+    path("<int:pk>/",      views.DetailView.as_view(), name="detail"),
+    path("<int:id>/edit/", views.edit,                 name="edit"),
 ]
--- devices/views.py
+++ devices/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import loader, render, get_object_or_404
-from django.http      import HttpResponse, Http404
+from django.shortcuts import get_object_or_404
+from django.http      import HttpResponseRedirect
 from django.views     import generic
+from django.urls      import reverse
 
 # Add model import
 from .models import Device
@@ -23,5 +24,23 @@ class DetailView(generic.DetailView):
     template_name = "devices/details.html"
     model         = Device
 
+
+# ==========================================
+# Edit a device given its ID
+# ==========================================
+def edit(request, id):
+
+    # Get device from ID
+    device = get_object_or_404(Device, pk = id)
+
+    # Get form data
+    new_name = request.POST['name']
+
+    # Update object in DB
+    device.name = new_name
+    device.save()
+
+    # Redirect back to the device list (TODO: Update redirect)
+    return HttpResponseRedirect(reverse("devices:index"))
 
     
