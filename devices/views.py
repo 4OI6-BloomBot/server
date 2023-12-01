9c5dc1de7a8eb854eeeb42682b84518d82d5493e
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2023-11-30 22:59:50 -0500

    Add device list as root of app

--- devices/views.py
+++ devices/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import loader
 from django.http      import HttpResponse
 
-# Create your views here.
+# Add model import
+from .models import Device
+
+
 def index(request):
-    return HttpResponse("Devices index")
+    device_list = Device.objects.order_by("name")[:5]
+    
+    # Create template and pass data to it
+    template = loader.get_template("devices/index.html")
+    context  = {
+        "device_list" : device_list
+    }
+
+    return HttpResponse(template.render(context, request))
