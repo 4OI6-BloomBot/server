8df9f7571bffe1e8d7e5a3e39603fe71f67aac3f
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2023-12-02 23:10:45 -0500

    Add Sensor and Measurement models

--- sensor_data/models.py
+++ sensor_data/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+# =============================
+# Sensor type model
+# =============================
+class Sensor(models.Model):
+  name      = models.CharField(max_length = 200)
+  unit      = models.CharField(max_length = 50)
+  precision = models.FloatField()
+
+  # Override string method to return the given name
+  def __str__(self):
+    return self.name
+
+
+# =============================
+# Measurement log model
+# =============================
+class Measurement(models.Model):
+  datetime  = models.DateTimeField(
+                auto_now     = False,
+                auto_now_add = False
+              )
+  
+  # Reference to the sensor type
+  # Do not allow sensor types to be deleted if they are being used
+  sensor    = models.ForeignKey(
+                Sensor,
+                on_delete = models.PROTECT 
+              )
+  
+  # Reference to the device that created the data
+  # When the device is deleted, delete the sensor data
+  device    = models.ForeignKey(
+                "devices.Device",
+                on_delete = models.CASCADE
+              )
+  
+  value     = models.FloatField()
+
+  # Override string method to return the given name
+  def __str__(self):
+    return self.device.name + "_" + self.sensor.name
+  
