7b2ade77b419b46f9901aa5be14465a54c060617
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2023-12-03 22:40:51 -0500

    Add per device location page

--- location/urls.py
+++ location/urls.py
@@ -8,7 +8,7 @@ from . import views
 
 app_name    = 'location' # Register namespace
 urlpatterns = [
-    path("",               views.IndexView.as_view(),  name="index"),
-    # path("<int:pk>/",      views.DetailView.as_view(), name="detail"),
+    path("",                 views.IndexView.as_view(),  name="index"),
+    path("device/<int:pk>/", views.DeviceView.as_view(), name="device"),
     # path("<int:id>/edit/", views.edit,                 name="edit"),
 ]
--- location/views.py
+++ location/views.py
@@ -12,3 +12,14 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Location.objects.order_by('datetime')[:5]
+
+
+# ==========================================
+# Device location history
+# ==========================================
+class DeviceView(generic.ListView):
+    template_name       = "location/index.html"
+    context_object_name = "location_list"
+
+    def get_queryset(self):
+        return Location.objects.filter(pk = self.kwargs.get('pk')).order_by('datetime')[:5]
