473d8f6d45b7291d16f0d6e95a11d3fa7bd2f0f6
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2023-12-09 23:03:09 -0500

    Add per sensor view for measurements

--- sensor_data/urls.py
+++ sensor_data/urls.py
@@ -9,6 +9,6 @@ from . import views
 app_name    = 'sensor_data' # Register namespace
 urlpatterns = [
     path("",               views.IndexView.as_view(),  name="index"),
-    # path("<int:pk>/",      views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/",      views.SensorView.as_view(), name="sensor"),
     # path("<int:id>/edit/", views.edit,                 name="edit"),
 ]
--- sensor_data/views.py
+++ sensor_data/views.py
@@ -1,7 +1,7 @@
 from django.views     import generic
 
 # Add model import
-from .models import Measurement
+from .models import Measurement, Sensor
 
 # ==========================================
 # Root of app. Fetch a list of all devices
@@ -17,8 +17,39 @@ class IndexView(generic.ListView):
         context['page_category'] = "sensor_data"
         context['page_title']    = "sensor data"
 
+        context['sensor_list'] = getSensors()
+
         return context
 
 
     def get_queryset(self):
         return Measurement.objects.order_by('datetime')[:5]
+
+
+# ==========================================
+# Display sensor specific data
+# ==========================================
+class SensorView(generic.ListView):
+    template_name       = "sensor_data/index.html"
+    context_object_name = "measurement_list"
+
+    # Add the page type for nav
+    def get_context_data(self, *args, **kwargs):
+        context = super(generic.ListView, self).get_context_data(*args, **kwargs)
+
+        context['page_category'] = "sensor_data"
+        context['page_title']    = Sensor.objects.get(pk = self.kwargs['pk']).name + " data"
+
+        context['sensor_list'] = getSensors()
+
+        return context
+
+
+    def get_queryset(self):
+        return Measurement.objects.filter(sensor = self.kwargs['pk']).order_by('datetime')
+
+
+
+
+def getSensors():
+    return Sensor.objects.order_by('name')
