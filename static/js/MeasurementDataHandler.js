d813574417dc9ca0e09783c25eeb4edb4cf0aaed
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2024-01-09 21:06:27 -0500

    Fix functionality of data handler class

--- static/js/MeasurementDataHandler.js
+++ static/js/MeasurementDataHandler.js
@@ -8,9 +8,14 @@ class MeasurementDataHandler {
   // Constructor
   // ==============================================
   constructor(callback) {
+    // ========================================
     // Config params
+    // ========================================
     this.fetch_interval   = 5000;
 
+    // ========================================
+    // Class vars
+    // ========================================
     this.all_data         = [];
     this.device_datasets  = [];
 
@@ -20,14 +25,23 @@ class MeasurementDataHandler {
     // Function to call when there is updated
     // data available
     this.callback_fn      = callback;
+
+    // ========================================
+    // Start the fetch loop
+    // ========================================
+    this.fetch_loop = setInterval(this.fetchMeasurements.bind(this), this.fetch_interval);
+    
   }
 
 
   // ==============================================
-  // Constructor
+  // Fetch data from the API
   // TODO: Change addresss to configurable route
   // ==============================================
   fetchMeasurements() {
+    // Create a reference to this obj to reference in AJAX success fn.
+    var self = this;
+
     $.ajax({
       url:      "http://localhost:8000/api/measurements/", 
       method:   'GET',
@@ -42,18 +56,20 @@ class MeasurementDataHandler {
 
         // Skip updates if there is no new data
         if (d.length > 0) {
-          parseDeviceData(d);
+          self.parseDeviceData(d);
 
           // Update the last received ID
           // Request data is ordered by received time
-          this.last_received_id = d[d.length - 1].id;
+          self.last_received_id = d[d.length - 1].id;
 
-          updateChart();
-          updateTable(d);
+          // Trigger the callback function with the new
+          // raw data.
+          self.callback_fn(d);
         }
       }
     });
   }
+
 
   // ==============================================
   // Find each of the unique devices from the data 
@@ -61,25 +77,26 @@ class MeasurementDataHandler {
   // for each device.
   // ==============================================
   parseDeviceData(d) {
-    devices = [...new Set(d.map(item => item.device))];
+    var filtered_data = [];
+    var devices       = [...new Set(d.map(item => item.device))];
 
     // Separate the data by device ID for each dataset
-    for (i = 0; i < devices.length; i++) {
+    for (var i = 0; i < devices.length; i++) {
       filtered_data = d.filter(item => item.device == devices[i]);
-      all_data.push(...filtered_data);
+      this.all_data.push(...filtered_data);
 
       // Search the array for the object w/ the current device ID
       // dataset_index will be equal to the index of the object if it exists.
       // Undefined if not.
-      dataset_index = device_data.findIndex(obj => { 
+      var dataset_index = this.device_datasets.findIndex(obj => { 
           return obj.label === devices[i];
       });
 
 
       // Append the new data if the object already exists, otherwise create
       // the object.
-      if (dataset_index != -1) appendMeasurementData(dataset_index, filtered_data);
-      else                     createDatasetObj(devices[i],         filtered_data);
+      if (dataset_index != -1) this.appendMeasurementData(dataset_index, filtered_data);
+      else                     this.createDatasetObj(devices[i],         filtered_data);
     }
   }
 
@@ -90,14 +107,14 @@ class MeasurementDataHandler {
   // ==============================================
   createDatasetObj(label, data) {
     // Create a new object for the device data
-    device_data.push(
+    this.device_datasets.push(
       {
         label: label,
         data:  []
       }
     );
 
-    appendMeasurementData(device_data.length - 1, data);
+    this.appendMeasurementData(this.device_datasets.length - 1, data);
   }
 
 
@@ -110,13 +127,13 @@ class MeasurementDataHandler {
     // Iterate over each entry and parse data for chart format
     // TODO: This needs to be updated to add data in order of time. 
     //       Otherwise, the chart will go back over itself.
-    for (j = 0; j < data.length; j++) {
-        device_data[index].data.push(
-            {
-                x: data[j].datetime, // TODO: Needs to be converted
-                y: data[j].value
-            }
-        );
+    for (var j = 0; j < data.length; j++) {
+      this.device_datasets[index].data.push(
+        {
+          x: data[j].datetime, // TODO: Needs to be converted
+          y: data[j].value
+        }
+      );
     }
   }
 
@@ -125,14 +142,14 @@ class MeasurementDataHandler {
   // Function to set the HTML parsing args
   // ==============================================
   setSensorID(id) {
-    sensor_id = id;
+    this.sensor_id = id;
 
     // Reset data and last received id
-    device_data      = [];
-    table_data       = [];
-    last_received_id = null;
+    this.device_datasets  = [];
+    this.all_data         = [];
+    this.last_received_id = null;
 
-    fetchMeasurements();
+    this.fetchMeasurements();
   }
 
 }
