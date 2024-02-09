690c69d6049dfe0f14efe6d02895c162b186f5c4
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2024-02-08 20:58:31 -0500

    Add Config to admin panel

--- bloombot_server/devices/admin.py
+++ bloombot_server/devices/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models        import Device
+from .models        import Device, Config
 
 # =========================================
 # Register the app models with the project
 # =========================================
 admin.site.register(Device)
+admin.site.register(Config)
